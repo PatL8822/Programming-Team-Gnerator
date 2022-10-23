c87d6e7327093248f9166b0b02c11e29a94f2f57
Isaiah411013 <Archangel_r320@hotmail.com>
2022-10-23 15:59:50 -0700

    added js files testing for file path connection

---
+++ Js/employee.js
@@ -0,0 +1,5 @@
+function employee(a, b) {
+    return a + b;
+};
+
+module.exports = employee();
---
+++ Js/test/employee.test.js
@@ -0,0 +1,6 @@
+
+const employee = require('../Js/employee')
+
+it('Should add to numbers', () => {
+    expect(employee(1, 2)).toBe(3)
+});
